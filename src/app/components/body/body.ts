e3b861152a5b225a2aba287ac8ff7d390fe396fe
Wissem Derouiche <Wissem.derouiche@isen.yncrea.fr>
2026-03-31 09:49:16 +0200

    add Header Body Footer

--- src/app/components/body/body.ts
+++ src/app/components/body/body.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-body',
-  imports: [],
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './body.html',
   styleUrl: './body.css',
 })
-export class Body {}
+export class BodyComponent {}
